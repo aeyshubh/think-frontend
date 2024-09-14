1c8fc89fe3a6a76100aa19992ed5d739769afbb9
Shubham Patel <shubhamashishpatel@gmail.com>
2024-09-15 06:07:32 +0800

    added frontend

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,26 @@
+"use client";
+import {
+  VStack,
+  Text,
+  Container,
+  HStack,
+  Box,
+  Show,
+  Link,
+  Flex,
+} from "@chakra-ui/react";
+
+import { DiscordButton } from "./components/DiscordButton";
+import { TelegramButton } from "./components/TelegramButton";
+import { Socials } from "./components/Socials";
+import { PRIVACY_POLICY_LINK, TERMS_AND_CONDITIONS_LINK } from "../../const";
+import { BeBetterVeBetterIcon } from "../Icon";
+
+export const Footer: React.FC = () => {
+
+
+
+  return (
+    <> </>
+  );
+};
